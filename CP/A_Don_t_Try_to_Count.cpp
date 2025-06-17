41c8c2e57c76ad302d99eb37aae63230264655c9
Sanskar Kansal <sanskarkansal42@gmail.com>
2025-06-17 19:40:21 +0530

    cf ques

---
+++ CP/A_Don_t_Try_to_Count.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+// Modular Exponentiation (a^b % mod)
+long long modPow(long long base, long long exp, long long mod) {
+    long long result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) result = (result * base) % mod;
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return result;
+}
+
+// GCD and LCM
+long long gcd(long long a, long long b) { return b == 0 ? a : gcd(b, a % b); }
+long long lcm(long long a, long long b) { return (a / gcd(a, b)) * b; }
+
+// Sieve of Eratosthenes
+vector<bool> sieve(int n) {
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = false;
+    isPrime[1] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i)
+                isPrime[j] = false;
+        }
+    }
+    return isPrime;
+}
+
+int main() {
+    fastIO;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,m;
+        cin>>n>>m;
+        string x,s;
+        cin>>x>>s;
+        int ans=0;
+        while(n<m){
+            x+=x;
+            n+=n;
+            ans++;
+        }
+        if (x.find(s) != string::npos) {
+            cout << ans << endl;
+        } 
+        else{
+            x+=x;
+            n+=n;
+            ans++;
+            if(x.find(s) != string::npos) {
+                cout << ans << endl;
+            } else {
+                cout << -1 << endl;
+            }
+        }
+    }
+    return 0;
+}
